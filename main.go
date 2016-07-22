12dbe000d171ece1e650a80e9d0cdadd313742a2
Max Gonzih <gonzih@gmail.com>
2016-07-22 09:30:46 +0200

    Renaming vars and etc

--- main.go
+++ main.go
@@ -33,19 +33,19 @@ func checkErr(err error) {
 }
 
 var format string
-var nginxLogFile string
+var inputLogFile string
 var logFile string
 var prefix string
-var logType string
+var inputFileType string
 var ratio int64
 var debug bool
 
 func init() {
 	flag.StringVar(&format, "format", `$remote_addr [$time_local] "$request" $status $request_length $body_bytes_sent $request_time "$t_size" $read_time $gen_time`, "Log format")
-	flag.StringVar(&nginxLogFile, "file", "-", "Log file name to read. Read from STDIN if file name is '-'")
+	flag.StringVar(&inputLogFile, "file", "-", "Log file name to read. Read from STDIN if file name is '-'")
 	flag.StringVar(&logFile, "log", "-", "File to report timings to, default is stdout")
 	flag.StringVar(&prefix, "prefix", "http://localhost", "Url prefix to query")
-	flag.StringVar(&logType, "log-type", "nginx", "Log type (nginx or haproxy)")
+	flag.StringVar(&inputFileType, "file-type", "nginx", "Input log type (nginx or haproxy)")
 	flag.Int64Var(&ratio, "ratio", 1, "Replay speed ratio, higher means faster replay speed")
 	flag.BoolVar(&debug, "debug", false, "Print extra debugging information")
 
@@ -144,20 +144,20 @@ func main() {
 	var inputReader io.Reader
 
 	if debug {
-		log.Printf("Parsing %s log file\n", nginxLogFile)
-		log.Printf("Using format %s", format)
+		log.Printf("Parsing %s log file\n", inputLogFile)
+		log.Printf("Using log type %s", inputFileType)
 	}
 
-	if nginxLogFile == "dummy" {
-		if logType == "nginx" {
+	if inputLogFile == "dummy" {
+		if inputFileType == "nginx" {
 			inputReader = strings.NewReader(`89.234.89.123 [08/Nov/2013:13:39:18 +0000] "GET /t/100x100/foo/bar.jpeg HTTP/1.1" 200 1027 2430 0.014 "100x100" 10 1`)
 		} else {
 			inputReader = strings.NewReader(`<142>Sep 27 00:15:57 haproxy[28513]: 67.188.214.167:64531 [27/Sep/2013:00:15:43.494] frontend~ test/10.127.57.177-10000 449/0/0/13531/13980 200 13824 - - ---- 6/6/0/1/0 0/0 "GET / HTTP/1.1"`)
 		}
-	} else if nginxLogFile == "-" {
+	} else if inputLogFile == "-" {
 		inputReader = os.Stdin
 	} else {
-		file, err := os.Open(nginxLogFile)
+		file, err := os.Open(inputLogFile)
 
 		checkErr(err)
 		defer file.Close()
@@ -167,13 +167,13 @@ func main() {
 
 	var reader LogReader
 
-	switch logType {
+	switch inputFileType {
 	case "nginx":
 		reader = NewNginxReader(inputReader, format)
 	case "haproxy":
 		reader = NewHaproxyReader(inputReader)
 	default:
-		log.Fatalf("log-type can be either haproxy or nginx, not '%s'", logType)
+		log.Fatalf("file-type can be either haproxy or nginx, not '%s'", inputFileType)
 	}
 
 	logWg.Add(1)
